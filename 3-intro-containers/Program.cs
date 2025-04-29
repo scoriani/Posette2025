ffb5142368907482c9ccdac740f62c111c81bb33
Silvano Coriani <scoriani@microsoft.com>
2025-04-29 15:27:27 +0200

    add CI/CD

--- 3-intro-containers/Program.cs
+++ 3-intro-containers/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+// Checking the OS and Framework information
+Console.WriteLine("OS and Framework Information:");
 Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.OSDescription);
 Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.OSArchitecture);
 Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier);
